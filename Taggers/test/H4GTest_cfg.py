501040199096b4f49bdaf9ae9eaf41256fa693ff
wamorkart <wamorkar.t@husky.neu.edu>
2019-07-11 19:54:14 +0200

    add option to run on condor

--- Taggers/test/H4GTest_cfg.py
+++ Taggers/test/H4GTest_cfg.py
@@ -1,7 +1,6 @@
 import subprocess
 import FWCore.ParameterSet.Config as cms
 import os
-# from FWCore.ParameterSet.VarParsing import VarParsing
 import FWCore.ParameterSet.VarParsing as VarParsing
 
 from flashgg.Taggers.flashggH4GCandidate_cfi import FlashggH4GCandidate
@@ -9,19 +8,10 @@ from flashgg.Taggers.flashggPreselectedDiPhotons_cfi import flashggPreselectedDi
 from flashgg.Taggers.flashggPreselectedDiPhotons_LowMass_cfi import flashggPreselectedDiPhotonsLowMass
 import flashgg.Taggers.dumperConfigTools as cfgTools
 
-# from secFiles import secFileList
-
-# print secFileList
-
 process = cms.Process("FLASHggH4GTest")
 
 
 from flashgg.MetaData.JobConfig import customize
-# customize.options.register('inputdataset',
-#                 '',
-#                 VarParsing.VarParsing.multiplicity.list,
-#                 VarParsing.VarParsing.varType.string,
-#                 "Input dataset(s)")
 
 customize.options.register('stdDumper',
                  '',
@@ -41,17 +31,14 @@ customize.options.register('vtxProbDumper',
                  VarParsing.VarParsing.varType.bool,
                  "vtxProbDumper")
 
-# customize.options.register('PURW',
-# 		          1,
-# 		          VarParsing.VarParsing.multiplicity.singleton,
-# 		          VarParsing.VarParsing.varType.bool,
-# 		          "Do PU reweighting")
+customize.options.register('isCondor',
+                 '',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "isCondor")
+
 
 from flashgg.MetaData.JobConfig import customize
-
-# customize.setDefault("maxEvents",-1)
-# customize.setDefault("targetLumi",1e+3)
-# customize.setDefault("puTarget",'2.39e+05,8.38e+05,2.31e+06,3.12e+06,4.48e+06,6e+06,7e+06,1.29e+07,3.53e+07,7.87e+07,1.77e+08,3.6e+08,6.03e+08,8.77e+08,1.17e+09,1.49e+09,1.76e+09,1.94e+09,2.05e+09,2.1e+09,2.13e+09,2.15e+09,2.13e+09,2.06e+09,1.96e+09,1.84e+09,1.7e+09,1.55e+09,1.4e+09,1.24e+09,1.09e+09,9.37e+08,7.92e+08,6.57e+08,5.34e+08,4.27e+08,3.35e+08,2.58e+08,1.94e+08,1.42e+08,1.01e+08,6.9e+07,4.55e+07,2.88e+07,1.75e+07,1.02e+07,5.64e+06,2.99e+06,1.51e+06,7.32e+05,3.4e+05,1.53e+05,6.74e+04,3.05e+04,1.52e+04,8.98e+03,6.5e+03,5.43e+03,4.89e+03,4.52e+03,4.21e+03,3.91e+03,3.61e+03,3.32e+03,3.03e+03,2.75e+03,2.47e+03,2.21e+03,1.97e+03,1.74e+03,1.52e+03,1.32e+03,1.14e+03,983,839')
 customize.parse()
 
 ###---H4G candidates production
@@ -123,10 +110,6 @@ process.h4gCandidateDumper = h4gCandidateDumper.clone()
 process.h4gCandidateDumper.dumpTrees = True
 process.h4gCandidateDumper.dumpWorkspace = False
 
-# process.h4gCandidateDumper.puReWeight=cms.bool(bool(customize.PURW))
-# if customize.PURW == False:
-#     process.h4gCandidateDumper.puTarget = cms.vdouble()
-
 cfgTools.addCategories(process.h4gCandidateDumper,
                        [
                             ("Reject", "", -1),
@@ -138,12 +121,10 @@ cfgTools.addCategories(process.h4gCandidateDumper,
                         variables = all_variables,
                         histograms=[]
                         )
-from flashgg.MetaData.JobConfig import customize
+# from flashgg.MetaData.JobConfig import customize
 
 # files = []
 # secondary_files = []
-# files = ['/store/group/phys_higgs/HiggsExo/H4Gamma/MicroAOD/H4G_Jun7/v0/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/Test_jun7-R2S16MAODv2-PUM17_GT/170607_180035/0000/myMicroAODOutputFile_9.root','/store/group/phys_higgs/HiggsExo/H4Gamma/MicroAOD/H4G_Jun7/v0/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/Test_jun7-R2S16MAODv2-PUM17_GT/170607_180035/0000/myMicroAODOutputFile_8.root']
-# secondary_files = ['/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/4EB38CC4-8DC8-E611-8B4E-F832E4CC4E51.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/56121268-20C9-E611-9FC8-00266CFB9534.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/6C3EB31D-0CC7-E611-BC45-02163E012E44.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/6CFF7ADF-83C8-E611-8A14-008CFA0A5740.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/9A344F39-25C8-E611-9A75-0CC47A706DC0.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/A60237A4-1FC9-E611-9195-848F69FD29B2.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/A8CBC588-3CC8-E611-8BE1-FA163E61F211.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/C85D3FF6-84C8-E611-B11D-D4AE526A0B29.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/56D830B2-83C8-E611-A696-FA163EB2D67F.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/F28A073D-9CC8-E611-9302-0025905C975C.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/56D830B2-83C8-E611-A696-FA163EB2D67F.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/56121268-20C9-E611-9FC8-00266CFB9534.root','/store/mc/RunIISummer16MiniAODv2/SUSYGluGluToHToAA_AToGG_M-60_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/4EB38CC4-8DC8-E611-8B4E-F832E4CC4E51.root']
 
 # for d in customize.inputdataset:
 #     print('>> Creating list of files from: \n'+d)
@@ -192,29 +173,8 @@ process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("test.root"),
                                    closeFileFast = cms.untracked.bool(True)
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-##--import flashgg customization
-# from flashgg.MetaData.JobConfig import customize
-# import FWCore.ParameterSet.VarParsing as VarParsing
-
-##--set default options
-# customize.setDefault("maxEvents",-1)
-# customize.setDefault("targetLumi",1e+3)
-# customize.register('PURW',
-# 				1,
-# 				VarParsing.VarParsing.multiplicity.singleton,
-# 				VarParsing.VarParsing.varType.bool,
-# 				"Do PU reweighting? Doesn't work on 76X")
-
-##--call the customization
-# customize(process)
-# process.h4gCandidateDumper.puReWeight=cms.bool(bool(customize.PURW))
-# if customize.PURW == False:
-	# process.h4gCandidateDumper.puTarget = cms.vdouble()
-#customize.setDefault("puTarget",'2.39e+05,8.38e+05,2.31e+06,3.12e+06,4.48e+06,6e+06,7e+06,1.29e+07,3.53e+07,7.87e+07,1.77e+08,3.6e+08,6.03e+08,8.77e+08,1.17e+09,1.49e+09,1.76e+09,1.94e+09,2.05e+09,2.1e+09,2.13e+09,2.15e+09,2.13e+09,2.06e+09,1.96e+09,1.84e+09,1.7e+09,1.55e+09,1.4e+09,1.24e+09,1.09e+09,9.37e+08,7.92e+08,6.57e+08,5.34e+08,4.27e+08,3.35e+08,2.58e+08,1.94e+08,1.42e+08,1.01e+08,6.9e+07,4.55e+07,2.88e+07,1.75e+07,1.02e+07,5.64e+06,2.99e+06,1.51e+06,7.32e+05,3.4e+05,1.53e+05,6.74e+04,3.05e+04,1.52e+04,8.98e+03,6.5e+03,5.43e+03,4.89e+03,4.52e+03,4.21e+03,3.91e+03,3.61e+03,3.32e+03,3.03e+03,2.75e+03,2.47e+03,2.21e+03,1.97e+03,1.74e+03,1.52e+03,1.32e+03,1.14e+03,983,839')
-
-# customize(process)
 if customize.inputFiles:
     inputFile = customize.inputFiles
 
@@ -258,4 +218,5 @@ if customize.vtxProbDumper:
    #vtxProb dumper sequence
    process.path = cms.Path(process.vtxH4GSequence*process.dataRequirements*process.FlashggH4GCandidate*process.h4gCandidateDumper_vtxProb)
 
-customize(process)
+if customize.isCondor:
+   customize(process)
